050dc522a0627c70cfd23482a9a7306d86372049
venizeng <venizeng@gmail.com>
2015-10-08 20:00:27 +0800

    提交HelloSpring代码

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(Application.class);
+		app.setShowBanner(false);
+		app.run(args);
+	}
+}
