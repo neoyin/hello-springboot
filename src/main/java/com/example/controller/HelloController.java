7868dd2e5ea8eaf7d5196d5bc5b290a9a1dc354d
venizeng <venizeng@gmail.com>
2015-10-27 16:07:23 +0800

    chang datas

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/hello")
 public class HelloController {
 
+	@Value("${sze-hello-world}")
+	private String name;
+	
+	@Value("${sze-hello-commonname}")
+	private String commonname;
+	
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public String hello(){
-		return "hello zengguangwei";
+		return name + commonname;
 	}
 }
